b0e6be804871e80db215c8217806258b3d672755
Jin827 <25231717+Jin827@users.noreply.github.com>
2018-08-12 13:21:27 +0900

    Ignoring node module libraries with server side webpack

--- webpack.server.js
+++ webpack.server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base.js');
+const webpackNodeExternals = require('webpack-node-externals');
 
 const config = {
   // Inform webpack that we're building a bundle for nodeJS, rather than for the browser
@@ -9,7 +10,9 @@ const config = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build')
-  }
+  },
+  // Not bundle node module libraries into the server side bundle ( make node modules require at server runtime) to reduce the initial webpack startup time.
+  externals: [webpackNodeExternals()]
 };
 
 module.exports = merge(baseConfig, config);
